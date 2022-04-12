684209b8d8cb68e16c6ea056bb59f5dccb35611c
joonseo12321 <joonseo12321@naver.com>
2022-04-12 22:25:43 +0900

    project

--- js/todo.js
+++ js/todo.js
@@ -23,7 +23,7 @@ function paintToDo(newTodo) {
   const span = document.createElement("span");
   span.innerText = newTodo.text;
   const button = document.createElement("button");
-  button.innerText = "ⓧ";
+  button.innerText = "remove";
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
