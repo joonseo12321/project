5e64877890132654a420bc228efb147567969c4b
joonseo12321 <joonseo12321@naver.com>
2022-04-14 12:26:05 +0900

    design

--- js/weather.js
+++ js/weather.js
@@ -7,11 +7,11 @@ function onGeoOk(position) {
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      });
 }
 function onGeoError() {
   alert("Can't find you. No weather for you.");
